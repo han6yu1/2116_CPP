82fe33845db97f48c14c524fd7ad0015c0735148
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-04-11 09:55:32 +0900

    setter

--- 2116_CPP/클래스 기본.cpp
+++ 2116_CPP/클래스 기본.cpp
@@ -27,6 +27,20 @@ class Student {
 		id = 2116;
 		age = 18;
 	 }
+
+	 // class는 멤버변수를 가질 수 있다
+	 void print() {
+		 cout << "** 학생정보 **" << endl << "- " << name << endl << "- " << sex << endl
+			 << "- " << department << endl << "- " << id << endl << "- " << age << "세" << endl;
+	 }
+
+	 void set_name(string _name) { name = _name; }
+	 void set_id(int _id) { id = _id; }
+	 void set_age(int _age) { age = _age; }
+	 void set_sex(int _sex) { sex = _sex; }
+	 void set_department(string _department) { department = _department; }
+
+private:
 	string name;
 	string department;
 	// id를 string으로 하지않는 이유
@@ -41,8 +55,6 @@ class Student {
 		: name(_name), id(_id), age(_age), sex(_sex), department(_department)
 	{
 
-		
-
 		// this를 하는 이유
 		// this : 객체 자기자신을 가리키는 포인터
 		// 매개변수명과 멤버변수명이 같아도 구별이 가능하다. 장점!
@@ -54,11 +66,6 @@ class Student {
 
 	}
 
-	// class는 멤버변수를 가질 수 있다
-	void print() {
-		cout << "** 학생정보 **" << endl << "- " << name << endl << "- " << sex << endl
-			<< "- " << department << endl << "- " << id << endl << "- " << age << "세" << endl;
-	}
 };
 
 int  main() {
@@ -77,18 +84,30 @@ int  main() {
 
 	// 정적할당 : 메모리의 크기가 컴파일 할 때 결정됨
 	//Student stu1 = Student("뉴진스", 2100, 18, 1, "뉴미디어소프트웨어");
-	Student stu3[2];
-	for (int i = 0; i < 2; i++) stu3[i].print();
+	//Student stu3[2];
+	//for (int i = 0; i < 2; i++) stu3[i].print();
 	
 	// 동적할당 : 메모리의 크기가 실행할 때 결정됨. heap영역
 	// c++의 동적할당은 반드시 delete로 해제해야 메모리 누수를 막을 수 있음
 	//Student* stu2 = new Student("비수얼", 3100, 31, 0, "방송연예과"); // 자바와 다르게 *를 씀
 	//stu2->print(); // 단일 객체는 멤버를 -> 로 접근
 	//delete stu2;
-	Student* stu4 = new Student[6];
-	for (int i = 0; i < 2; i++) stu4[i].print(); // 배열의 요소에 해당하는 객체는 멤버를 .으로 표현한다.
+	//Student* stu4 = new Student[6];
+	//for (int i = 0; i < 2; i++) stu4[i].print(); // 배열의 요소에 해당하는 객체는 멤버를 .으로 표현한다.
+	//delete[] stu4; // 배열 delete하기
 
-	delete[] stu4; // 배열 delete하기
+	Student* stu4 = new Student[2];
+	stu4[0].set_age(18);
+	stu4[0].set_name("한별");
+	stu4[0].set_id(2116);
+	stu4[0].set_sex(1);
+	stu4[0].set_department("뉴미디어 소프트웨어");
+
+	stu4[1].set_age(18);
+	stu4[1].set_name("혁수");
+	stu4[1].set_id(2118);
+	stu4[1].set_sex(0);
+	stu4[1].set_department("뉴미디어 소프트웨어");
 
 	return 0;
 }
