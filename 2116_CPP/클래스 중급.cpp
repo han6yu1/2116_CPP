c635611caed9784fc0a0d8647eb156dfe0d124f2
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-04-20 00:37:19 +0900

    동적할당

--- 2116_CPP/클래스 중급.cpp
+++ 2116_CPP/클래스 중급.cpp
@@ -1,26 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
-using namespace std;
-
-// 클래스 안에 문자열을 할당할 순 없고, 대신 포인터 변수를 넣음(불가능은 아닌데 아주 비효율적이다)
-#include <iostream>
+#include <cstring> // strlen, strcpy 함수를 사용하기 위해 필요
 
 using namespace std;
 
 class MString {
 public:
     MString(const char* str) {
-        c_str_ = str;
-        size_ = 0;
-        while (str[size_]) {
-            size_++;
-        }
+        unsigned int str_length = strlen(str);
+        // 필요한 길이만큼 문자열 동적할당
+        c_str_ = new char[str_length + 1]; // '/0' Null a문자 공간 +1
+        strcpy(c_str_, str);
+
+        size_ = str_length; // size_ 변수 초기화
     }
+
     unsigned int size() { return size_; }
-    const char* c_str() { return c_str_; }
+    char* c_str() { return c_str_; }
+
 private:
     int size_; // 문자열의 길이
-    const char* c_str_; // 문자열을 가리키는 주소, c_str_ 적는거 법칙
+    char* c_str_; // 문자열을 가리키는 주소, c_str_ 적는거 법칙
 };
 
 int main() {
@@ -28,3 +28,4 @@ int main() {
     cout << str.c_str() << endl;
     return 0;
 }
+// 아니 이거 왜 오류나??
