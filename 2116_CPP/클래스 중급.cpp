eec12fda5c1e91244faf4984486c9151dfd87bce
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-04-25 08:47:34 +0900

    소멸자로 동적할당한 공간을 해제

--- 2116_CPP/클래스 중급.cpp
+++ 2116_CPP/클래스 중급.cpp
@@ -11,13 +11,17 @@ public:
         // 필요한 길이만큼 문자열 동적할당
         c_str_ = new char[str_length + 1]; // '/0' Null a문자 공간 +1
         strcpy(c_str_, str);
-
-        size_ = str_length; // size_ 변수 초기화
     }
 
     unsigned int size() { return size_; }
     char* c_str() { return c_str_; }
+    
+    // 소멸자(destructor)
+    ~MString() {
+        // 생성자에 동적할당한 공간을 해제
+        delete[] c_str_;
 
+    }
 private:
     int size_; // 문자열의 길이
     char* c_str_; // 문자열을 가리키는 주소, c_str_ 적는거 법칙
