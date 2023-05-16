32a6b58093dd0901f9f371dae5ee9c37305fe3e8
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-05-16 09:12:54 +0900

    복사생성자를 얕은복사로 해서 문제가 발생

---
+++ 2116_CPP/복사생성자.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Munja {
+public:
+	// 일반생성자 (깊은복사)
+	Munja(const char* str) {
+		// 3("abc") + 1('\0')
+		str_ = new char[strlen(str) + 1];
+		
+		strcpy(str_, str); // str의 내용을 str_에 복사
+		cout << "일반생성자 호출" << endl;
+	}
+
+	// 복사생성자(얕은 복사로 인하여 문제점이 발생)
+	Munja(const Munja& m) : str_(m.str_) {
+		cout << "복사생성자 호출" << endl;
+	}
+
+	// 소멸자
+	~Munja() {
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
+private:
+	char* str_;
+};
+
+
+int main(void) {
+	Munja m1 = Munja("abc"); // 일반생성자 호출
+	Munja m2 = m1; // 복사생성자 호출
+}
