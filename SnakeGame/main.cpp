62ce9b2564648e3c809cda29a939ed5bdee064ff
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-05-18 11:15:36 +0900

    [SFML] 프레임 조절

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -5,7 +5,16 @@ int main() {
 	using namespace sf;
 
 	RenderWindow window(VideoMode(640, 480), "Snake Game");
+	// 1초에 60번의 작업이 이루어 지도록 frame 조절
+	// 컴퓨터 사양이 달라도 똑같은 속도로 처리함
+	window.setFramerateLimit(60);
 	
+	RectangleShape snake;
+	snake.setPosition(200, 300);
+	snake.setSize(Vector2f(30, 30));
+	snake.setFillColor(Color::Green);
+
+
 	while (window.isOpen()) { // window창 계속 유지
 		Event e;
 		while (window.pollEvent(e)) {
@@ -13,7 +22,13 @@ int main() {
 			if (e.type == Event::Closed)
 				window.close();
 		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+			snake.move(0, -1); // 컴퓨터에선 y값이 위로 갈수록 감소임
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+			snake.move(0, 1);
 		window.clear();
+		window.draw(snake);
 		window.display();
 	}
 
