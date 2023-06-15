d1e047323c7f0a690da642f0929d4303cda1d948
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-06-15 10:23:02 +0900

    다형성 동적바인딩

--- 2116_CPP/상속.cpp
+++ 2116_CPP/상속.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
-#define COLOR_RED 0
-#define COLOR_GREEN 1
-#define COLOR_BLUE 2
+#define COLOR_RED		0
+#define COLOR_GREEN		1
+#define COLOR_BLUE		2
+
 
 class Animal {
 public:
 	Animal(int color, int age) : color_(color), age_(age)
-	{ 
-		cout << "Animal 생성자()" << endl; 
-	} 
-	~Animal() { cout << "Animal 소멸자()" << endl; }
-	void Bite() { cout << "Animal 물다" << endl; }
-	void Eat() { cout << "Animal 먹다" << endl; }
-	void Sleep() { cout << "Animal 자다" << endl; }
-
-	void Roar(void) {
-		cout << "Roar" << endl;
+	{
+		cout << "Animal 생성자()" << endl;
 	}
+	~Animal() { cout << "Animal 소멸자()" << endl; }
+	// 가상함수 : Java처럼 동적바인딩
+	virtual void Roar(void) { cout << "Animal 짖다" << endl; }
+	void Eat(void) { cout << "Animal 먹다" << endl; }
+	void Sleep(void) { cout << "Animal 자다" << endl; }
 
 private:
 	int color_;
@@ -27,31 +24,33 @@ private:
 };
 
 class Rabbit : public Animal {
-public :
+public:
 	// 부모생성자(Animal(color, age))가 먼저 호출
-	Rabbit(int color, int age, int ear_length) : Animal(color, age), ear_length_(ear_length_)
+	Rabbit(int color, int age, int ear_length) : Animal(color, age), ear_length_(ear_length)
 	{
-		cout << "Rabbit 생성자()" << endl; 
+		cout << "Rabbit 생성자()" << endl;
 	}
 	~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
 
-	// 함수 ovveride
-	// 다형성(polymorphism) : 같은 함수 시그니처(반환형, 이름 매개변수가 모두 같은)가 다르게 실행되는 것
-	void Roar(void) {
+	// 함수 override
+	// 다형성(polymorphism) : 시그니쳐(반환형, 이름, 매개변수가 모두 같은)가 같은 함수임에도 
+	// 불구하고 다르게 실행되는 것
+	void Roar(void)
+	{
 		cout << "깡총깡총" << endl;
 	}
+
 private:
 	int ear_length_;
 };
 
-int main(void) {
-
+int main(void)
+{
 	Animal* animal = new Animal(COLOR_BLUE, 30);
-	animal->Roar(); // 동물 짖어
-	Rabbit* rabbit = new Rabbit(COLOR_RED, 3, 20); 
-	animal->Roar();
+	animal->Roar();	// 동물 짖어
+	delete animal;
 
-	delete rabbit;
-	// rabbit을 생성하기 위해서 Animal이 같이 호출된다.
-	
+	animal = new Rabbit(COLOR_RED, 3, 20);
+	animal->Roar();	// 깡총 깡총
+	delete animal;
 }
