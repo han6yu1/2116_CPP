85b462bbbb8a2bc8d7648ad2797be675718b26ba
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-08-17 11:13:36 +0900

    템플릿클래스

---
+++ Inheritance/템플릿클래스.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class MyData {
+public: 
+	MyData(T data) : data_(data) {
+
+	}
+	T get_data(void) {
+		return data_;
+	}
+	void set_data(T data) {
+		data_ = data;
+	}
+private:
+	T data_;
+};
+
+
+int main() {
+	
+	MyData<int> md = MyData<int>(3);
+	cout << md.get_data() << endl;
+
+	return 0;
+}
