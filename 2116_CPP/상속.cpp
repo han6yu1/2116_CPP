58f9e4636dc0a087638bb1ca14d54c05adc13594
hanbyul0725 <s2234@e-mirim.hs.kr>
2023-08-22 07:50:38 +0900

    [상속] 적 하나 만들기

--- 2116_CPP/상속.cpp
+++ 2116_CPP/상속.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+
 
 using namespace sf;
 
@@ -37,6 +40,8 @@ public:
 
 int main(void)
 {
+	srand((unsigned int)time(NULL));
+
 	RenderWindow window(VideoMode(1000, 800), "Sangsok");
 	window.setFramerateLimit(60);
 
@@ -45,7 +50,13 @@ int main(void)
 	sp1.setSize(Vector2f(50, 50));
 	sp1.setFillColor(Color::Blue);
 
+	RectangleShape se1;
+	se1.setPosition(rand() % 800, rand() % 600);
+	se1.setSize(Vector2f(40, 40));
+	se1.setFillColor(Color::Yellow);
+
 	Entity* player = new Entity(3, 5, &sp1);
+	Entity* enemy1 = new Entity(1, 3, &se1);
 
 	while (window.isOpen())
 	{
@@ -77,6 +88,7 @@ int main(void)
 		// Render
 		window.clear();
 
+		window.draw(enemy1->get_sprite());
 		window.draw(player->get_sprite());
 
 		window.display();
--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,50 +1,96 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+
 
 using namespace sf;
 
 class Entity {
+public:
 	// 거의 99.99%이상 클래스를 매개변수로 할 때는 주소값으로 받자
-	// 메모리 용량 및 call by value 이슈때문.
-	Entity(int life, RectangleShape* sprite)
-		: life_(life), sprite_(sprite) {
-		
+	// 메모리 용량 및 call by value 이슈 때문에
+	Entity(int life, int speed, RectangleShape* sprite)
+		: life_(life), sprite_(sprite), speed_(speed)
+	{
 	}
-	~Entity(){}
-	
+
+	~Entity() {}
+
+	// 스프라이트의 위치를 x, y만큼 변경
+	void move(float x, float y)
+	{
+		sprite_->move(x, y);
+	}
+
 	// getter
 	int get_life() { return life_; }
-	RectangleShape* get_sprite() { return *sprite_; }
+	int get_speed() { return speed_; }
+	RectangleShape get_sprite() { return *sprite_; }
 
 	// setter
 	void set_life(int val) { life_ = val; }
+	void set_speed(int val) { speed_ = val; }
 	void set_sprite(RectangleShape* val) { sprite_ = val; }
 
-private:
+	//private:
 	int life_;
+	int speed_;
 	RectangleShape* sprite_;
-
 };
 
-int main(void) {
+int main(void)
+{
+	srand((unsigned int)time(NULL));
 
 	RenderWindow window(VideoMode(1000, 800), "Sangsok");
-	
+	window.setFramerateLimit(60);
 
 	RectangleShape sp1;
 	sp1.setPosition(400, 300);
-	sp1.setSize(Vector2f(300, 300));
+	sp1.setSize(Vector2f(50, 50));
 	sp1.setFillColor(Color::Blue);
 
-	Entity* player = new Entity(3, &sp1);
+	RectangleShape se1;
+	se1.setPosition(rand() % 800, rand() % 600);
+	se1.setSize(Vector2f(40, 40));
+	se1.setFillColor(Color::Yellow);
 
-	while (window.isOpen()) {
+	Entity* player = new Entity(3, 5, &sp1);
+	Entity* enemy1 = new Entity(1, 3, &se1);
+
+	while (window.isOpen())
+	{
 		Event e;
-		while (window.pollEvent(e)) {
+		while (window.pollEvent(e))
+		{
 			if (e.type == Event::Closed)
 				window.close();
 		}
+		// Input
+		// 방향키 이동
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			player->move(0, -player->get_speed());
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			player->move(0, player->get_speed());
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			player->move(player->get_speed(), 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			player->move(-player->get_speed(), 0);
+		}
+
+
+		// Update
+
+
+		// Render
 		window.clear();
-		window.draw(player -> )
+
+		window.draw(enemy1->get_sprite());
+		window.draw(player->get_sprite());
+
 		window.display();
 	}
 	return 0;
